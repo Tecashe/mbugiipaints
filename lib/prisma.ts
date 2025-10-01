f2a0305ec89ac090a0c2c70af5d8fe9a38290ebe
Macharia11 <stevieskaka3096@gmail.com>
2025-10-01 16:18:49 +0300

    simples

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,13 @@
+// import { PrismaClient } from "@prisma/client"
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined
+// }
+
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
 import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = globalThis as unknown as {
@@ -5,5 +15,7 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+export const client = prisma
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
